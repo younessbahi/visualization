8f61b69e51d80e8704e8ba7f11ff178338361f83
younessbahi <77124184+younessbahi@users.noreply.github.com>
2021-10-26 12:42:38 +0100

    Update main.R

--- maps/dubaiBuilding/main.R
+++ maps/dubaiBuilding/main.R
@@ -7,23 +7,24 @@ if (!require(victor)) {
 
 ## IMPORT DATA VIA API ---
 # dubai <-
-# 	tribble(
-#		~city, ~lon, ~lat,
-# 		"Dubai", 55.296249,25.276987
-# 	) %>%
-# 		purrr::transpose()
+# tribble(
+# 	~city, ~lon, ~lat,
+# 	"Dubai", 55.296249,25.276987
+# ) %>% purrr::transpose()
 #
 # names(dubai) <-
-# 		purrr::map_chr(dubai, "city")
-# cityData <- purrr::map(
-# 		dubai,
-# 		~ victor::spoils(
-#				.x$lon,
-# 		                .x$lat,
-# 		                 zoom = 15,
-# 		                 nrow = 25,
-# 		                 ncol = 14
-# 		)
+# purrr::map_chr(dubai, "city")
+
+# cityData <- 
+# purrr::map(
+# 	dubai,
+# 	~ victor::spoils(
+#                	.x$lon,
+# 		        .x$lat,
+# 		        zoom = 15,
+# 		        nrow = 25,
+# 		        ncol = 14
+# 	)
 # )
 # saveRDS(cityData, 'cityData.RDS')
 
@@ -41,59 +42,51 @@ mapBuildings <-
 				mutate(height_fct = cut(height, breaks = 5)),
 		        aes(fill = height_fct), colour = NA
 		) +
-		guides(fill = guide_legend(title = NULL, override.aes = list(size = 1, alpha = 1))) +
+
+guides(fill = guide_legend(title = NULL, override.aes = list(size = 1, alpha = 1))) +
 		ggthemes::scale_fill_tableau(palette = "Jewel Bright", direction = -1) +
 		theme_void() +
 		theme(
-			panel.background = element_rect(colour = NA, fill = "#0a0a0a"),
-			plot.background = element_rect(colour = NA, fill = "#0a0a0a"),
-			plot.margin = margin(0, 2, 0, 2),
-			legend.key.size = unit(.2, 'cm'),
-			legend.key.height = unit(.2, 'cm'), #change legend key height
-			legend.key.width = unit(1, 'cm'), #change legend key width
-			legend.position = "bottom" ,#c(0.01, 0.02),
-			legend.background = element_blank(),
-			legend.key = element_blank(),
-			legend.text = element_text(colour = "white"),
-			legend.margin = margin(-15,0,3,0),
-			legend.box.background = element_rect(color = NA, fill = NA),
-			legend.direction = "horizontal",
-			legend.justification = c(0, 0)
+				panel.background = element_rect(colour = NA, fill = "#203e4a"),
+				plot.background = element_rect(colour = NA, fill = "#203e4a"),
+				plot.margin = margin(0, 0, 0, 0),
+				legend.key.size = unit(.1, 'cm'),
+				legend.key.height = unit(.2, 'cm'), #change legend key height
+				legend.key.width = unit(1, 'cm'), #change legend key width
+				legend.position = c(0.01, 0.02),
+				legend.background = element_blank(),
+				legend.key = element_blank(),
+				legend.text = element_text(colour = "white"),
+				legend.margin = margin(-15,0,3,0),
+				legend.box.background = element_rect(color = NA, fill = NA),
+				legend.direction = "horizontal",
+				legend.justification = c(0, 0)
 		)
 
-ggsave(plot = mapBuildings, here("dubai_buildings.png"), width = 8, height = 10, dpi = 300)
 
-source('plot.R')
+ggsave(plot = mapBuildings, here("dubai_buildings.png"), width = 4.5,dpi = 300)
 main_image <- magick::image_read("dubai_buildings.png")
-plots <- magick::image_crop(main_image, "2200x3000+50+0")
-legends <- magick::image_crop(main_image,geometry = "2000x245+0-200" , gravity = "northwest")
-
-
-magick::image_append(c(plots, legends), stack = TRUE) %>%
+main_image %>%
 		magick::image_annotate(
 				text = "Dubai",
 				weight = 900,
 				gravity = "northwest",
-				color = "#d9c09e",
-				size = 120,
+				color = "white",
+				size = 80,
 				location = "+20+20"
 		) %>%
 		magick::image_annotate(
-				text = "City Building Height Distribution (> 30m)",
+				text = "Building Height Distribution (> 30m)",
 				gravity = "northwest",
-				color = "#d9c09e",
-				size = 45,
-				location = "+20+150"
+				color = "white",
+				size = 35,
+				location = "+20+100"
 		) %>%
 		magick::image_annotate(
 				text = "Visualization By: Youness Bahi",
-				gravity = "northeast",
-				color = "#d3dadc",
-				size = 35,
-				location = "+20+150"
+				gravity = "southeast",
+				color = "white",
+				size = 30,
+				location = "+42+100"
 		) %>%
-		magick::image_composite(
-				composite_image = densityPlot,
-				offset = "+0+300"
-		) %>%
-		image_write(here("Dubai_buildings_height.png"))
+		image_write(here("Dubai_buildings_height.png"), quality = 100)
